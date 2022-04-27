a1d06ff1a0e1001bf83aca25360dcde40caa8964
Nikkolet <nashby@gmail.com>
2022-04-27 17:52:23 -0600

    commit

--- week02/tictactoe.py
+++ week02/tictactoe.py
@@ -1,135 +1,62 @@
 # Assignment: W02 Prove: Developer - Solo Code Submission
 # Author: Nikkolet Ashby
 # Purpose: Program plays Tic Tac Toe game for enjoyment of two users. 
-# Date: 4/25/2022
+# Date: 4/27/2022
 
 #=======================================Modules============================================
-from tkinter import *
-import random
+
 #==========================================================================================
+
 def main():
-    pass
+    player = next_player("")
+    board = create_board()
+    while not (has_winner(board) or is_a_draw(board)):
+        display_board(board)
+        make_move(player, board)
+        player = next_player(player)
+    display_board(board)
+    print("Good game. Thanks for playing!") 
 
-def next_turn(row, column):
+def create_board():
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
 
-    global player
-
-    if buttons[row][column]['text'] == "" and check_winner() is False:
-
-        if player == players[0]:
-
-            buttons[row][column]['text'] = player
-
-            if check_winner() is False:
-                player = players[1]
-                label.config(text=(players[1]+" turn"))
-
-            elif check_winner() is True:
-                label.config(text=(players[0]+" wins"))
-
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
-        else:
-
-            buttons[row][column]['text'] = player 
-
-            if check_winner() is False:
-                player = players[0]
-                label.config(text=(players[0]+ " turn"))
-
-            elif check_winner() is True:
-                label.config(text=(players[1]+ "wins!"))
-            
-            elif check_winner() == "Tie":
-                label.config(text="Tie!")
-
-
-def check_winner():
-
-    for row in range(3):
-        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
-            buttons[row][0].config(bg="green")
-            buttons[row][1].config(bg="green")
-            buttons[row][2].config(bg="green")
-            return True
+def display_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
     
-    for column in range(3):
-        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
-            buttons[0][column].config(bg="green")
-            buttons[1][column].config(bg="green")
-            buttons[2][column].config(bg="green")
-            return True
-
-    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons [2][2]['text'] != "":
-        buttons[0][0].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][2].config(bg="green")
-        return True
-
-    elif buttons[0][2]['text'] == buttons[1][1]['text'] == buttons [2][0]['text'] != "":
-        buttons[0][2].config(bg="green")
-        buttons[1][1].config(bg="green")
-        buttons[2][0].config(bg="green")
-        return True
+def is_a_draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True 
     
-    elif empty_spaces() is False:
+def has_winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6])
 
-        for row in range(3):
-            for column in range(3):
-                buttons[row][column].config(bg="yellow")
-        return "Tie"
+def make_move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
 
-    else:
-        return False
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
 
-
-def empty_spaces():
-
-    spaces = 9
-
-    for row in range(3):
-        for column in range(3):
-            if buttons[row][column]['text'] != "":
-                spaces-= 1
-
-    if spaces == 0:
-        return False
-    else: 
-        return True
-    
-
-def new_game():
-    global player
-
-    player = random.choice(players)
-
-    label.config(text = player + " turn")
-
-    for row in range(3):
-        for column in range(3):
-            buttons[row][column].config(text="",bg="#F0F0F0")
-
-
-
-window = Tk()
-window.title("Tic Tac Toe")
-players = ["X","O"]
-player = random.choice(players)
-buttons = [[0,0,0],
-          [0,0,0],
-          [0,0,0]]
-
-label = Label(text= player + " turn", font=('consolas',40))
-label.pack(side="top")
-
-reset_button = Button(text="restart", font=('consolas',20), command = new_game)
-reset_button.pack(side="top")
-
-frame = Frame(window)
-frame.pack()
-
-for row in range(3):
-    for column in range(3):
-        buttons[row][column] = Button(frame, text="",  font=('consolas',40), width=5, height=2, command = lambda row=row, column=column: next_turn(row, column))
-        buttons[row][column].grid(row=row, column=column)
-window.mainloop()
+if __name__ == "__main__":
+    main()
