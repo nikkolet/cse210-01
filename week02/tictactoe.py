2312b8c37e6a979403e70bdc90e9ad15c60a0be1
Nikkolet <nashby@gmail.com>
2022-04-29 22:07:26 -0600

    commit

--- week02/tictactoe.py
+++ week02/tictactoe.py
@@ -85,123 +85,123 @@ def checkIfWon():
     #Is X the winner?
     #Row
     if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
-        b1.config(bg="red")
-        b2.config(bg="red")
-        b3.config(bg="red")
+        b1.config(bg="green")
+        b2.config(bg="green")
+        b3.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     elif b4["text"] == "X" and b5["text"] == "X" and b6["text"] == "X":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
+        b4.config(bg="green")
+        b5.config(bg="green")
+        b6.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     elif b7["text"] == "X" and b8["text"] == "X" and b9["text"] == "X":
-        b7.config(bg="red")
-        b8.config(bg="red")
-        b9.config(bg="red")
+        b7.config(bg="green")
+        b8.config(bg="green")
+        b9.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     #Column
     elif b1["text"] == "X" and b4["text"] == "X" and b7["text"] == "X":
-        b1.config(bg="red")
-        b4.config(bg="red")
-        b7.config(bg="red")
+        b1.config(bg="green")
+        b4.config(bg="green")
+        b7.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     elif b2["text"] == "X" and b5["text"] == "X" and b8["text"] == "X":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
+        b4.config(bg="green")
+        b5.config(bg="green")
+        b6.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     elif b3["text"] == "X" and b6["text"] == "X" and b9["text"] == "X":
-        b7.config(bg="red")
-        b8.config(bg="red")
-        b9.config(bg="red")
+        b7.config(bg="green")
+        b8.config(bg="green")
+        b9.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     #Diagonal
     elif b1["text"] == "X" and b5["text"] == "X" and b9["text"] == "X":
-        b1.config(bg="red")
-        b5.config(bg="red")
-        b9.config(bg="red")
+        b1.config(bg="green")
+        b5.config(bg="green")
+        b9.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
     elif b3["text"] == "X" and b5["text"] == "X" and b7["text"] == "X":
-        b3.config(bg="red")
-        b5.config(bg="red")
-        b7.config(bg="red")
+        b3.config(bg="green")
+        b5.config(bg="green")
+        b7.config(bg="green")
         winner = True
-        messagebox.showinfo("X wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "X wins!")
         disableButtons()
 
     # Is O the winner?
     #Row
     elif b1["text"] == "O" and b2["text"] == "O" and b3["text"] == "O":
-        b1.config(bg="red")
-        b2.config(bg="red")
-        b3.config(bg="red")
+        b1.config(bg="green")
+        b2.config(bg="green")
+        b3.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
+        b4.config(bg="green")
+        b5.config(bg="green")
+        b6.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
-        b7.config(bg="red")
-        b8.config(bg="red")
-        b9.config(bg="red")
+        b7.config(bg="green")
+        b8.config(bg="green")
+        b9.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     #Column
     elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
-        b1.config(bg="red")
-        b4.config(bg="red")
-        b7.config(bg="red")
+        b1.config(bg="green")
+        b4.config(bg="green")
+        b7.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
-        b4.config(bg="red")
-        b5.config(bg="red")
-        b6.config(bg="red")
+        b4.config(bg="green")
+        b5.config(bg="green")
+        b6.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
-        b7.config(bg="red")
-        b8.config(bg="red")
-        b9.config(bg="red")
+        b7.config(bg="green")
+        b8.config(bg="green")
+        b9.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     #Diagonals
     elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
-        b1.config(bg="red")
-        b5.config(bg="red")
-        b9.config(bg="red")
+        b1.config(bg="green")
+        b5.config(bg="green")
+        b9.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
-        b3.config(bg="red")
-        b5.config(bg="red")
-        b7.config(bg="red")
+        b3.config(bg="green")
+        b5.config(bg="green")
+        b7.config(bg="green")
         winner = True
-        messagebox.showinfo("O wins!", "Congratulations!!!")
+        messagebox.showinfo("Congratulations!!!", "O wins!")
         disableButtons()
     elif count == 9 and winner == False:
         b1.config(bg="red")
@@ -213,7 +213,7 @@ def checkIfWon():
         b7.config(bg="red")
         b8.config(bg="red")
         b9.config(bg="red")
-        messagebox.showinfo("Tie!", "So Close!!!")
+        messagebox.showinfo("So Close!!!", "Tie!")
         disableButtons() 
        
 
