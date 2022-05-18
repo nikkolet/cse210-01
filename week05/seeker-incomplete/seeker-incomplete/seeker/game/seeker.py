61a7aa5199c9fad89a1ef033775aacc9d2d12881
nikkolet <81433602+nikkolet@users.noreply.github.com>
2022-05-18 03:52:15 -0600

    commit

--- week05/seeker-incomplete/seeker-incomplete/seeker/game/seeker.py
+++ week05/seeker-incomplete/seeker-incomplete/seeker/game/seeker.py
@@ -1,6 +1,7 @@
 # TODO: Implement the Seeker class as follows...
 
 # 1) Add the class declaration. Use the following class comment.
+class Seeker:
 
     """The person looking for the Hider. 
     
@@ -11,6 +12,8 @@
     """
 
 # 2) Create the class constructor. Use the following method comment.
+    def __init__(self) -> None:
+        pass
         """Constructs a new Seeker.
 
         Args:
@@ -18,16 +21,20 @@
         """
        
 # 3) Create the get_location(self) method. Use the following method comment.
+    def get_location(self):
         """Gets the current location.
         
         Returns:
             number: The current location,
         """
+        return self.get_location
         
 # 4) Create the move_location(self, location) method. Use the following method comment.
+    def move_location(self, location):
         """Moves to the given location.
 
         Args:
             self (Seeker): An instance of Seeker.
             location (int): The given location.
         """
+        self.move_location
