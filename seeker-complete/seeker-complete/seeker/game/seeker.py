a5ab3f7f69152343b6c34c5db18998ae744f99bf
nikkolet <81433602+nikkolet@users.noreply.github.com>
2022-05-19 17:08:55 -0600

    commit

--- seeker-complete/seeker-complete/seeker/game/seeker.py
+++ seeker-complete/seeker-complete/seeker/game/seeker.py
@@ -1,40 +1,31 @@
-# TODO: Implement the Seeker class as follows...
-from random import random
-
-
 import random
-# 1) Add the class declaration. Use the following class comment.
-class Seeker:
 
-    """The person looking for the Hider. 
+class Seeker:
+    """The person looking for the Hider . 
     
     The responsibility of a Seeker is to keep track of its location and distance travelled.
     
     Attributes:
         location (int): The location of the Seeker (1-1000).
+        distance (List[int]): The distances travelled.
     """
-   
 
-# 2) Create the class constructor. Use the following method comment.
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
         """Constructs a new Seeker.
 
         Args:
             self (Seeker): An instance of Seeker.
         """
-        self._location = random.randint(1,1000)
-       
-# 3) Create the get_location(self) method. Use the following method comment.
+        self._location = random.randint(1, 1000)
+        
     def get_location(self):
         """Gets the current location.
         
         Returns:
             number: The current location,
         """
-        return self.get_location()
+        return self._location
         
-# 4) Create the move_location(self, location) method. Use the following method comment.
     def move_location(self, location):
         """Moves to the given location.
 
@@ -42,4 +33,4 @@ class Seeker:
             self (Seeker): An instance of Seeker.
             location (int): The given location.
         """
-        self.move_location = location
+        self._location = location
